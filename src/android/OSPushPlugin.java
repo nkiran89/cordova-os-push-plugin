1c7d31d11ba4c6aa33899f44e888387252c7fe78
nkiran89 <kiran2272@gmail.com>
2025-06-13 16:02:46 +0530

    Initial commit

---
+++ src/android/OSPushPlugin.java
@@ -0,0 +1,36 @@
+package com.example.ospushplugin;
+
+import android.widget.Toast;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class OSPushPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("onNotificationReceived")) {
+           
+            this.executeBackgroundLogic(callbackContext);
+            callbackContext.success();
+            return true;
+            return true;
+        }
+        return false;
+    }
+
+    private void executeBackgroundLogic( CallbackContext callbackContext) {
+         cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(cordova.getActivity(), "Hello World!!", 2000).show();
+            }
+        });
+       
+    }
+}
---
+++ www/OSPushPlugin.js
@@ -0,0 +1,10 @@
+var exec = require('cordova/exec');
+
+exports.onNotificationReceived = function (callback) {
+    exec(callback, null, 'OSPushPlugin', 'onNotificationReceived', []);
+};
+
+module.exports = function (ctx) {
+    console.log("RUNNING HOOK NOW!");    
+    throw new Error(`OUTSYSTEMS_PLUGIN_ERROR: Error occurred on ${ctx.hook}`)
+};
